fd6d518e207da8a687b24ac755d6729bced88d70
Dmytro_Tereshchenko <Dmytro_Tereshchenko@epam.com>
2016-02-29 13:06:28 +0200

    Fix build html template issue.

--- Gruntfile.js
+++ Gruntfile.js
@@ -470,7 +470,7 @@ module.exports = function (grunt) {
                     dest: '<%= yeoman.dist %>',
                     src: [
                         '*.{ico,png,txt}',
-                        '*.html',
+                        '{,*/|,**/}*.html',
                         'assets/images/{,*/|,**/}*.{webp}',
                         'assets/fonts/{,*/|,**/}*.*'
                     ]
