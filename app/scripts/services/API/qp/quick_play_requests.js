53780d281182ff88000e1b1c11ff3e38b8921f94
Anton Ovcharuk <Anton_Ovcharuk@epam.com>
2016-03-03 13:31:45 +0200

    In progress

--- app/scripts/services/API/qp/quick_play_requests.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Anton_Ovcharuk on 3/3/2016.
- */
--- app/scripts/services/API/quick_play_parsers.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Anton_Ovcharuk on 3/3/2016.
- */
-'use strict';
-
-angular.module('webPlayerApp').service('QuickPlayParsersService', function () {
-	var self = this;
-});
