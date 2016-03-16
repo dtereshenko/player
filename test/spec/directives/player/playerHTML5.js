06f19b4c7afdbec32e67003f610314f355e494c8
Maksym_Rudnyi <maksym_rudnyi@epam.com>
2016-03-16 11:07:24 +0200

    my little fixed test

--- test/spec/directives/player/playerHTML5.js
+++ test/spec/directives/player/playerHTML5.js
@@ -1,31 +1,19 @@
 describe('Unit testing great quotes', function() {
-	var $compile, $rootScope, directiveElem, $templateCache, $document, body;
+	var $scope, element;
 
 
 	beforeEach(module('webPlayerApp'));
 	beforeEach(module('views/player/playerThird.html'));
 
-	beforeEach(inject(function(_$compile_, _$rootScope_, _$templateCache_, _$document_){
-			//template = $templateCache.get('views/player/playerThird.html');
-			//$templateCache.put('views/player/playerThird.html',template);
-		$document = _$document_;
-		$compile = _$compile_;
-		$rootScope = _$rootScope_;
-		$templateCache = _$templateCache_;
-		body = $document.find('body');
-
-		directiveElem = $compile('<div player-third></div>')($rootScope);
-		$rootScope.$digest();
+	beforeEach(inject(function($rootScope){
+		$scope = $rootScope.$new();
 	}));
 
-	it('exposes the controller to the template', function() {
-		//$templateCache.get('views/player/playerThird.html', '<div></div>');
-		var scope = $rootScope.$new();
-
-		element = $compile('<div player-third></div>')(scope);
-		$rootScope.$digest();
-
-	});
+	it('exposes the controller to the template', inject(function ($compile) {
+		element = $compile('<div player-third></div>')($scope);
+		$scope.$digest();
+		expect(element.html()).toMatch(/Third player/);
+	}));
 
 
 	//it('should have span element', function () {
